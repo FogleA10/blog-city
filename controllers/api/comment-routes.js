d882c1c931cc0d912db9940155a944d21096109b
audreyfogle <FogleA10@gmail.com>
2022-06-16 15:08:59 -0400

    working on comments problem and setting up controllers

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,31 +1,111 @@
+// const router = require('express').Router();
+// const { Comment } = require('../../models/');
+// const withAuth = require('../../utils/auth');
+
+// // router.get('/', withAuth, async (req, res) => {
+// //   await Comment.findAll({})
+// //     .then(commentData => res.json(commentData))
+// //     .catch(err => {
+// //       console.log(err);
+// //       res.status(500).json(err);
+// //     })
+// // });
+
+// // router.get('/:id', withAuth, async (req, res) => {
+// //   await Comment.findAll({
+// //     where: {
+// //       id: req.params.id
+// //     }
+// //   })
+// //     .then(commentData => res.json(commentData))
+// //     .catch(err => {
+// //       console.log(err);
+// //       res.status(500).json(err);
+// //     });
+// // });
+
+
+
+
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newComment = await Comment.create({
+//       ...req.body,
+//       userId: req.session.userId,
+//     });
+//     res.json(newComment);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+// // router.put('/:id', withAuth, async (req, res) => {
+// //   await Comment.update({
+// //     comment: req.body.comment
+// //   }, {
+// //     where: {
+// //       id: req.params.id
+// //     }
+// //   })
+// //     .then(commentData => {
+// //       if (!commentData) {
+// //         res.status(404).json({ message: 'Comment with this ID not found' });
+// //         return;
+// //       }
+// //       res.json(commentData);
+// //   })
+// //     .catch(err => {
+// //     console.log(err);
+// //     res.status(500).json(err);
+// //   });
+// // });
+
+// // router.delete('/:id', withAuth, (req, res) => {
+// //   Comment.destroy({
+// //     where: {
+// //       id: req.params.id
+// //     }
+// //   })
+// //   .then(commentData => {
+// //     if (!commentData) {
+// //       res.status(404).json({ message: 'Comment with this ID not found' });
+// //       return;
+// //     }
+// //     res.json(commentData);
+// //   })
+// //   .catch(err => {
+// //     console.log(err);
+// //     res.status(500).json(err);
+// //   });
+// // });
+
+// module.exports = router;
 const router = require('express').Router();
-const { Comment } = require('../../models/');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  await Comment.findAll({})
-    .then(commentData => res.json(commentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    })
-});
+// router.get('/', (req, res) => {
+//     Comment.findAll({})
+//         .then(dbCommentData => res.json(dbCommentData))
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         })
+// });
 
-router.get('/:id', withAuth, async (req, res) => {
-  await Comment.findAll({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(commentData => res.json(commentData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-
-
+// router.get('/:id', (req, res) => {
+//     Comment.findAll({
+//             where: {
+//                 id: req.params.id
+//             }
+//         })
+//         .then(dbCommentData => res.json(dbCommentData))
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         })
+// });
 
 router.post('/', withAuth, async (req, res) => {
   try {
@@ -39,45 +119,54 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// router.post('/', withAuth, (req, res) => {
+//     if (req.session) {
+//         Comment.create({
+//                 comment_text: req.body.comment_text,
+//                 post_id: req.body.post_id,
+//                 user_id: req.session.user_id,
+//             })
+//             .then(dbCommentData => res.json(dbCommentData))
+//             .catch(err => {
+//                 console.log(err);
+//                 res.status(400).json(err);
+//             })
+//     }
+// });
 
-router.put('/:id', withAuth, async (req, res) => {
-  await Comment.update({
-    comment: req.body.comment
-  }, {
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(commentData => {
-      if (!commentData) {
-        res.status(404).json({ message: 'Comment with this ID not found' });
-        return;
-      }
-      res.json(commentData);
-  })
-    .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
+// router.put('/:id', withAuth, (req, res) => {
+//     Comment.update({
+//         comment_text: req.body.comment_text
+//     }, {
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(dbCommentData => {
+//         if (!dbCommentData) {
+//             res.status(404).json({ message: 'No comment found with this id' });
+//             return;
+//         }
+//         res.json(dbCommentData);
+//     }).catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 
-router.delete('/:id', withAuth, (req, res) => {
-  Comment.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(commentData => {
-    if (!commentData) {
-      res.status(404).json({ message: 'Comment with this ID not found' });
-      return;
-    }
-    res.json(commentData);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
-});
-
+// router.delete('/:id', withAuth, (req, res) => {
+//     Comment.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(dbCommentData => {
+//         if (!dbCommentData) {
+//             res.status(404).json({ message: 'No comment found with this id' });
+//             return;
+//         }
+//         res.json(dbCommentData);
+//     }).catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
+// });
 module.exports = router;
